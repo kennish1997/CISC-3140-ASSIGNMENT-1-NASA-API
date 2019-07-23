6555abaa61d62f87d6411b99d51304ccfe43327c
kennish <49169064+kennish1997@users.noreply.github.com>
2019-07-23 18:19:39 -0400

    Add files via upload

---
+++ NasaAPI.py
@@ -0,0 +1,34 @@
+import urllib.request
+from urllib.request import urlopen
+import json
+import requests
+import ssl
+from flask import Flask,render_template
+import webbrowser
+
+app=Flask(__name__)
+myContext = ssl.SSLContext()
+
+apodurl= 'https://api.nasa.gov/planetary/apod?'
+mykey= 'api_key=FaBTaAnCYyPyLrXYkbiUjyBPJRJQmsgDPcsLRMM1'
+urlcom= apodurl + mykey
+link = urllib.request.urlopen(urlcom, context=myContext)
+apodread= link.read()
+decodeapod= json.loads(apodread.decode('utf-8'))
+myurl=decodeapod['hdurl']
+
+@app.route('/')
+def nasaapi():
+    return render_template('home.html',APOD= myurl)+ "<b>Title:</b>" + decodeapod['title'] + "<br>" +"<b>Date:</b>"  +decodeapod['date']+"<br>" "<b>Explanation:</b> " + "<br>" + decodeapod['explanation']
+ 
+   # response = requests.get(myurl)
+    #img = Image.open(BytesIO(response.content))
+    #webbrowser.open(decodeapod['url'],new=0, autoraise=True)+from urllib.request import urlopen
+
+    
+    
+
+
+app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
