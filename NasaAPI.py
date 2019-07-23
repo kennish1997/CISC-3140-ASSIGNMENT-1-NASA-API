9cd0e6ce95d67a5d60e5275bdd84eae34f80b49d
kennish <49169064+kennish1997@users.noreply.github.com>
2019-07-23 18:27:17 -0400

    Update NasaAPI.py

--- NasaAPI.py
+++ NasaAPI.py
@@ -1,3 +1,4 @@
+#Many are not needed I think...
 import urllib.request
 from urllib.request import urlopen
 import json
@@ -11,17 +12,21 @@ myContext = ssl.SSLContext()
 
 apodurl= 'https://api.nasa.gov/planetary/apod?'
 mykey= 'api_key=FaBTaAnCYyPyLrXYkbiUjyBPJRJQmsgDPcsLRMM1'
+#Basically the url stored in a simple variable
 urlcom= apodurl + mykey
 link = urllib.request.urlopen(urlcom, context=myContext)
+
 apodread= link.read()
 decodeapod= json.loads(apodread.decode('utf-8'))
+#The url of the jpeg
 myurl=decodeapod['hdurl']
 
 @app.route('/')
 def nasaapi():
+    #Taking the small details from the api such as the title, explanation, and date
     return render_template('page.html',APOD= myurl)+ "<b>Title:</b>" + decodeapod['title'] + "<br>" +"<b>Date:</b>"  +decodeapod['date']+"<br>" "<b>Explanation:</b> " + "<br>" + decodeapod['explanation']
     
-    
-app.run(debug=True)
+#When you save any changes on your file, you just have to refresh your page for changes to take place
+
 if __name__ == '__main__':
     app.run(debug=True)
